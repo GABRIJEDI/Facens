455145610c5dccffe853b3cebc8e8c24b04c95c5
Gabriel Antunes <gabrielzoldic@gmail.com>
2024-09-13 01:35:39 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,15 +5,31 @@ public class Main {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
 
-        int primeiroValor, segundoValor, total;
+        int primeiroValor, segundoValor, total = 0;
 
         System.out.println("Insira o primeiro valor: ");
         primeiroValor = scn.nextInt();
+        System.out.println("Insira a operação desejada (+, -, / ou *): ");
+        String op = scn.next();
         System.out.println("Insira o segundo valor: ");
         segundoValor = scn.nextInt();
 
-        total=primeiroValor+segundoValor;
+        if (op.equals ("+")){
+            total = primeiroValor+segundoValor;
+            System.out.println("O calculo de "+ primeiroValor + " "+ op + " "+ segundoValor + " É igual a: "+total);
+        }else if (op.equals ("-")){
+            total = primeiroValor-segundoValor;
+            System.out.println("O calculo de "+ primeiroValor + " "+ op + " "+ segundoValor + " É igual a: "+total);
+        }else if (op.equals ("/")){
+            total = primeiroValor/segundoValor;
+            System.out.println("O calculo de "+ primeiroValor + " "+ op + " "+ segundoValor + " É igual a: "+total);
+        }else if (op.equals("*")){
+            total = primeiroValor*segundoValor;
+            System.out.println("O calculo de "+ primeiroValor + " "+ op + " "+ segundoValor + " É igual a: "+total);
+        }
+        else{
+            System.out.println("Operador invalido");
+        }
 
-        System.out.println("O resutado e: "+ total);
-    }
+    }    
 }
