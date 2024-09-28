2e63dd75eae2099505d651b2f10d0ac7255b4857
Gabriel Antunes <gabrielzoldic@gmail.com>
2024-09-27 22:06:10 -0300

    Update Paresfor.java

--- Paresfor.java
+++ Paresfor.java
@@ -3,8 +3,6 @@ import java.util.Scanner;
 public class Paresfor{
 
     public static void main(String[] args){
-        Scanner scn = new Scanner(System.in);
-
         int i;
 
         for(i=0; i<=10; i=i+2){
