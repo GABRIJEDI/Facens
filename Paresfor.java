94b9ec0f43f63e160eeaee30c12b85a2bcfabad8
Gabriel Antunes <gabrielzoldic@gmail.com>
2024-09-27 22:05:21 -0300

    Add files via upload

---
+++ Paresfor.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Paresfor{
+
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+
+        int i;
+
+        for(i=0; i<=10; i=i+2){
+            System.out.println(""+i);
+        }
+    }
+}
